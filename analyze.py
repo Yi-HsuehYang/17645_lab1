1c657410c2dd3f41a133f2fe3b8517530348b8d2
Yi-HsuehYang <yihsuehy@andrew.cmu.edu>
2024-01-19 10:03:06 -0500

    initial commit

---
+++ analyze.py
@@ -0,0 +1,50 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time, os
+
+endpoint = os.environ.get('AZURE_COMPUTER_VISION_API_ENDPOINT')
+region = os.environ.get('AZURE_COMPUTER_VISION_REGION')
+key = os.environ.get('AZURE_COMPUTER_VISION_API_KEY')
+
+credentials = CognitiveServicesCredentials(key)
+
+client = ComputerVisionClient(
+    endpoint="https://" + region + ".api.cognitive.microsoft.com/",
+    credentials=credentials
+)
+
+def read_image(uri):
+    numberOfCharsInOperationId = 36
+    maxRetries = 10
+
+    # SDK call
+    rawHttpResponse = client.read(uri, language="en", raw=True)
+
+    # Get ID from returned headers
+    operationLocation = rawHttpResponse.headers["Operation-Location"]
+    idLocation = len(operationLocation) - numberOfCharsInOperationId
+    operationId = operationLocation[idLocation:]
+
+    # SDK call
+    result = client.get_read_result(operationId)
+    
+    # Try API
+    retry = 0
+    
+    while retry < maxRetries:
+        if result.status.lower () not in ['notstarted', 'running']:
+            break
+        time.sleep(1)
+        result = client.get_read_result(operationId)
+        
+        retry += 1
+    
+    if retry == maxRetries:
+        return "max retries reached"
+
+    if result.status == OperationStatusCodes.succeeded:
+        res_text = " ".join([line.text for line in result.analyze_result.read_results[0].lines])
+        return res_text
+    else:
+        return "error"
